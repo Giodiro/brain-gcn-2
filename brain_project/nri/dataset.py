Fixed trainer bugs in summary processing; fixed normalization

brain_project/nri/dataset.py
@@ -4,9 +4,10 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 import torch.nn as nn
-from sklearn.preprocessing import scale
+from sklearn.preprocessing import StandardScaler
 
 from cache import LRUCache
+from util import time_str
 
 
 all_subjects = ['S01', 'S03', 'S04',
@@ -213,11 +214,9 @@ class EEGDataset2(Dataset):
       (e.g. 1e-9) number to avoid precision issues.
     """
 
-    all_normalizations = ["spat-mean",     # Make each time-frame 0 mean
-                          "spat-standard", # Standardize each time-frame
-                          "temp-mean",     # Make each ROI 0 mean
-                          "temp-standard", # Standardize each ROI
-                          "none",          # Multiply all values by 1e9
+    all_normalizations = ["standard",     # Standardize each ROI
+                          "none",         # Multiply all values by NORM_CONSTANT
+                          "val",          # Indicates that this is a validation loader so normalization is loaded from the tr loader
                           ]
 
     NORM_CONSTANT = 1.0e10
@@ -249,18 +248,23 @@ class EEGDataset2(Dataset):
 
         self.file_indices = file_indices
 
-    def __getitem__(self, idx):
+        self.init_normalizer()
+
+    def get_xy_files(self, idx):
         idx = self.file_indices[idx]
         sdata = self.subj_data[idx]
         x_file = os.path.join(self.data_folder, "X", sdata["file"])
         y_file = os.path.join(self.data_folder, "Y", sdata["file"])
         iif = sdata["index_in_file"]
 
+        return x_file, y_file, iif
+        
+    def __getitem__(self, idx):
+        x_file, y_file, iif = self.get_xy_files(idx)
+
         X = self.xfile_cache.load(x_file, iif).transpose()
         X = self.normalize(X)
         Y = self.yfile_cache.load(y_file, iif) - 1  # Necessary, targets must start from 0
-
-        # TODO: Run normalization
 
         sample = {
             "X": torch.tensor(X, dtype=torch.float32),
@@ -282,6 +286,25 @@ class EEGDataset2(Dataset):
 
         return batch
 
+    def init_normalizer(self):
+        if self.normalization == "val":
+            return
+
+        print(f"{time_str()} Initializing normalization ({self.normalization}) statistics.")
+        if self.normalization == "none":
+            self.scaler = None
+            return
+
+        self.scaler = StandardScaler(copy=False, with_mean=True, with_std=True)
+        # Iterate all samples to compute statistics.
+        # TODO: This can be optimized to feed the scalers all samples read from a file
+        #       but care must be taken to actually only feed it samples whose id is in
+        #       the allowed ids.
+        for i in range(len(self)):
+            x_file, y_file, iif = self.get_xy_files(i)
+            X = self.xfile_cache.load(x_file, iif).transpose()
+            self.scaler.partial_fit(X)
+
     def normalize(self, data):
         """
         Args:
@@ -293,14 +316,8 @@ class EEGDataset2(Dataset):
 
         if self.normalization == "none":
             data = data * EEGDataset2.NORM_CONSTANT
-        elif self.normalization == "spat-mean":
-            data = scale(data, with_mean=True, with_std=False, axis=1)
-        elif self.normalization == "spat-std":
-            data = scale(data, with_mean=True, with_std=True, axis=1)
-        elif self.normalization == "temp-mean":
-            data = scale(data, with_mean=True, with_std=False, axis=0)
-        elif self.normalization == "temp-std":
-            data = scale(data, with_mean=True, with_std=True, axis=0)
+        else:
+            data = self.scaler.transform(data)
 
         return data.astype(np.float32)
 

brain_project/nri/train.py
@@ -34,7 +34,7 @@ log_path = "gen_data/logs/"
 # Subject list is used to restrict loaded data to just the listed subjects.
 subject_list = ["S01", "S02"]
 # This must be implemented in the dataset class (TODO)
-normalization = "none"
+normalization = "standard"
 # The number of nodes in each graph.
 num_atoms = 423
 # The number of time-steps per sample (this depends on the preprocessing).
@@ -81,7 +81,10 @@ print(f"{time_str()} Subject data contains {len(subj_data)} samples coming "
 
 num_workers = 3
 tr_dataset = EEGDataset2(data_folder, tr_indices, subj_data, normalization)
-val_dataset = EEGDataset2(data_folder, val_indices, subj_data, normalization)
+val_dataset = EEGDataset2(data_folder, val_indices, subj_data, normalization="val")
+# Normalization statistics should only be computed on the training set.
+val_dataset.normalization = tr_dataset.normalization
+val_dataset.scaler = tr_dataset.scaler
 
 tr_loader = data.DataLoader(tr_dataset,
                             shuffle=True,
@@ -200,7 +203,7 @@ def validate(epoch, keep_data=False):
 
     losses_kl = []; losses_rec = []
     if keep_data:
-        data = {"edges": [], "target": [], "preds": []}
+        data_dict = {"edges": [], "target": [], "preds": []}
 
     encoder.eval()
     decoder.eval()
@@ -238,25 +241,25 @@ def validate(epoch, keep_data=False):
         losses_rec.append(loss_rec.data.cpu().numpy())
 
         if keep_data:
-            data["edges"].append(edges)
-            data["target"].append(inputs["Y"].cpu().numpy())
-            data["preds"].append(output.data.cpu().numpy())
+            data_dict["edges"].append(edges.data.cpu().numpy())
+            data_dict["target"].append(inputs["Y"].data.cpu().numpy())
+            data_dict["preds"].append(output.data.cpu().numpy())
 
     loss_kl = np.mean(losses_kl)
     loss_rec = np.mean(losses_rec)
 
     if keep_data:
-        data["target"] = np.concatenate(data["target"])
-        data["preds"] = np.concatenate(data["preds"])
-        return loss_kl, loss_rec, data
+        data_dict["target"] = np.concatenate(data_dict["target"])
+        data_dict["preds"] = np.concatenate(data_dict["preds"])
+        return loss_kl, loss_rec, data_dict
 
     return loss_kl, loss_rec
 
-def training_summaries(data, epoch, summary_writer):
+def training_summaries(data_dict, epoch, summary_writer):
     import sklearn.metrics as metrics
 
-    targets = data["target"]
-    preds = np.argmax(data["preds"], axis=1)
+    targets = data_dict["target"]
+    preds = np.argmax(data_dict["preds"], axis=1)
 
     # Accuracy
     accuracy = metrics.accuracy_score(targets, preds, normalize=True)
@@ -280,7 +283,7 @@ for epoch in range(n_epochs):
     if keep_data:
         val_loss_kl, val_loss_rec, data_val = out_val
         st = time.time()
-        training_summaries(data,
+        training_summaries(data_val,
                            epoch,
                            summary_writer)
         print(f"{time_str()} Wrote summary data to tensorboard in {time.time() - st:.2f}s.")